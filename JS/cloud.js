18b46d2b38cb3d3abbab44d6cf4daa548388f5c7
subhamkr555 <subhamkr555@gmail.com>
2020-04-06 17:37:20 +0530

    Added the print msg animation

--- JS/cloud.js
+++ JS/cloud.js
@@ -1,16 +1,17 @@
-function printMsg(){
-    var msg = "WELCOME TO ITER ROBOTICS CLUB" ;
-    var tempMsg = " ";
-    for (let index = 0; index < msg.length;index++) {
-        tempMsg += msg[index];
-        document.getElementById("welcomeMessage").innerHTML = tempMsg;
-        setTimeout(delay(),1000);
-    }
+
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve,milliseconds))
 }
 
-function delay() {
-    for (let index = 0; index < 9999; index++) {
-        var element = index;
-        
-    }
+const doSomething = async() => {
+    var msg = "WELCOME TO ITER ROBOTICS CLUB";
+    var tmpMsg = " ";
+    for (let index = 0; index < msg.length; index++) {
+        tmpMsg += msg[index]; 
+        document.getElementById("welcomeMessage").innerHTML = tmpMsg;
+        await sleep(150)
+    } 
 }
+
+doSomething()
+
